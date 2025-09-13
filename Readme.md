278b46bd741993e07d989ada32a09178372cc169
Paras Maggu <pmaggu@adobe.com>
2025-09-13 15:33:16 +0530

    Basic Level code Undestanding foundation

--- Readme.md
+++ Readme.md
@@ -1,1 +1,19 @@
 # Making for practice a git 
+
+#Parcel
+- Dev build
+- Local server
+- HMR - (Hot module Replacement)
+- File watching algorithm - written in c++
+- Caching Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code spliting
+- Differential Bundling - support older version
+- Diagnostic
+- Error Handling 
+- HTTPS
+- Tree Shaking - remove unwanted code
--- app.js
+++ app.js
@@ -1,21 +1,43 @@
+import React  from "react"
+import ReactDOM from "react-dom/client"
 
+// React elemet
+// const heading = React.createElement("h1",{id: "heading", xy: "XYZ"},"First react program")
 
+// React elemet like this
 // <div id="parent">
 //     <div id="child">
 //         <h1>I'm H1 Tag</h1>
 //         <h1>I'm H2 Tag</h1>
 //     </div>
 // </div>
+// const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {},"I'm H1 Tag"), React.createElement("h2", {},"I'm H2 Tag")]))
 
 
-// const heading = React.createElement("h1",{id: "heading", xy: "XYZ"},"First react program")
+//JSX
+const heading = (<h1 id="New_ID" className="Classy" tabIndex="1">Hello Through JSX</h1>);
 
-const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {},"I'm H1 Tag"), React.createElement("h2", {},"I'm H2 Tag")]))
+//Class Component 
+//Functional Component: Normal JS Function(A function who return a JSX is a functional component)
 
-console.log(parent)
+//Functional Component 1:
+const Heading1 = () => (<h1 id="New_ID" className="Classy" tabIndex="1">Hello Through JSX</h1>);
+
+//Functional Component 2:
+const FunctionalComponent = () => {
+return <div id="Functonal_Composition">
+    <Heading1/>
+    <h1>Through Functional Component</h1>
+</div>
+}
+
+
+console.log('1',FunctionalComponent)
+console.log('2',FunctionalComponent())
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 console.log('Root',root)
 
-root.render(parent)
+//root.render(heading)
+root.render(<FunctionalComponent/>)
